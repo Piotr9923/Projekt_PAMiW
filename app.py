4f6edffac86e33f8ca9a98ddc8f9a27db67a5531
Your Name <p-rzewnicki@o2.pl>
2020-10-28 15:50:45 +0100

    Dodano walidację pól formularza

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__,static_url_path='/static')
 
 app.debug = False
 
-@app.route('/sender/sign-up')
+@app.route('/sender/sign-up', methods=["GET"])
 def registration():
     return render_template("registration.html")
 
@@ -15,4 +15,3 @@ def index():
 
 
 app.run()
-
---
+++ static/main.js
@@ -0,0 +1,127 @@
+
+document.getElementById("button").disabled = true;
+
+function attach_events(){
+
+    var form = document.getElementById("formularz");
+    var button = document.getElementById("button");
+    form.addEventListener("change",function(ev){
+
+        button.disabled = true;
+
+        if(validateFields()==true) button.disabled = false;
+   
+    })
+
+}
+
+function validateFields(){
+
+    var pl_letters = /^[A-Za-zĄĆĘŁŃÓŚŹŻąćęłńóśź]+$/;
+    var pl_big_letters = /^[A-ZĄĆĘŁŃÓŚŹŻ]+$/;
+    var big_letters = /^[A-Z]+$/;
+    var letters = /^[A-Za-z]+$/;
+    var password_letters = /^[A-Za-z]+$/;
+
+    is_correct=true;
+
+    var firstname = document.getElementById("firstname");
+    
+    if(firstname.value.match(pl_letters) && firstname.value[0].match(pl_big_letters) && /[a-ząćęłńóśź]/.test(firstname.value)){
+        firstname.classList.remove("incorrect_field");
+    }
+    else{
+        is_correct=false;
+        firstname.classList.add("incorrect_field");
+    }
+
+    var lastname = document.getElementById("lastname");
+    
+    if(lastname.value.match(pl_letters) && lastname.value[0].match(pl_big_letters) && /[a-ząćęłńóśź]/.test(lastname.value)){
+        lastname.classList.remove("incorrect_field");
+    }
+    else{
+        is_correct=false;
+        lastname.classList.add("incorrect_field");
+
+    }
+
+    var login = document.getElementById("login");
+    
+    if(login.value.match(letters) && login.value.length>2 && login.value.length<13){
+        login.classList.remove("incorrect_field");
+    }
+    else{
+        is_correct=false;
+        login.classList.add("incorrect_field");
+    }
+
+    var password = document.getElementById("password");
+
+    
+    if(password.value.match(password_letters) && /^[A-Z]/.test(password.value) && password.value.length>7){
+        password.classList.remove("incorrect_field");
+    }
+    else{
+        is_correct=false;
+        password.classList.add("incorrect_field");
+    }
+
+    var password_again = document.getElementById("password_again");
+    
+    if(password.value == password_again.value){
+        password_again.classList.remove("incorrect_field");
+    }
+    else{
+        is_correct=false;
+        password_again.classList.add("incorrect_field");
+    }
+
+    var male = document.getElementById("male");
+    var female = document.getElementById("female");
+
+    if(!(male.checked || female.checked)){
+        is_correct=false;
+    }
+
+    var photo = document.getElementById("photo")
+
+    if(photo.files[0] == undefined){
+
+        is_correct=false;
+    }
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET',"https://infinite-hamlet-29399.herokuapp.com/check/"+login.value,false);
+    xhr.onreadystatechange=function(){
+
+        var DONE =4;
+        var OK=200;
+
+        if(xhr.readyState == DONE){
+            if(xhr.status == OK){
+                console.log(xhr.responseText);
+            } else{
+                console.log("Error:" + xhr.status);
+            }
+        }
+
+
+    }
+    if(login.value.length>0){
+        xhr.send(null);
+
+        if(xhr.status>=200 && xhr.status<300){
+            
+            json=JSON.parse(xhr.response)
+            console.log(json.value);
+
+        }
+    }
+
+    return is_correct
+}
+
+
+
+attach_events();
