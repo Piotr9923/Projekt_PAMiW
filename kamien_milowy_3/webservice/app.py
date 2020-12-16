fa8852575a063eedaf211436dd042de382e57f68
Your Name <p-rzewnicki@o2.pl>
2020-12-16 14:30:05 +0100

    Usługa sieciowa

--- kamien_milowy_3/webservice/app.py
+++ kamien_milowy_3/webservice/app.py
@@ -4,7 +4,7 @@ from flask import render_template, flash, url_for
 from os import getenv
 from bcrypt import hashpw, gensalt, checkpw
 from redis import StrictRedis
-from datetime import datetime
+from datetime import datetime, timedelta
 from uuid import uuid4
 from jwt import encode, decode
 from redis.exceptions import ConnectionError
@@ -16,7 +16,7 @@ import json
 
 app = Flask(__name__)
 HAL(app)
-load_dotenv()
+load_dotenv('.env')
 
 REDIS_HOST = getenv("REDIS_HOST")
 REDIS_PASS = getenv("REDIS_PASS")
@@ -29,7 +29,7 @@ SESSION_TYPE = "redis"
 SESSION_REDIS = db
 SESSION_COOKIE_HTTPONLY = True
 JWT_SECRET = getenv("JWT_SECRET")
-
+JWT_TIME = 300
 app.config.from_object(__name__)
 app.secret_key = getenv("SECRET_KEY")
 
@@ -38,12 +38,14 @@ app.debug = False
 
 @app.before_request
 def before():
-    token = request.headers.get("Authorization","").replace("Bearer","")
+    token = request.headers.get("Authorization", "").replace("Bearer ", "")
+
     try:
-        g.authorization = decode(token, JWT_SECRET, algorithms=["HS256"])
-        print("Authorized "+str(g.authorization))
+        g.authorization = decode(token, str(JWT_SECRET), algorithms=["HS256"])
+
+        print("Authorized " + str(g.authorization))
     except Exception as e:
-        print("Unauthorized "+str(e))
+        print("Unauthorized " + str(e))
         g.authorization = {}
 
 
@@ -71,13 +73,12 @@ def is_user(login):
 
 
 def save_user(firstname, lastname, login, email, password, adress):
-
     salt = gensalt(5)
     password = password.encode()
     adress = adress.encode()
     email = email.encode()
     hashed = hashpw(password, salt)
-    db.hset(f"user:{login}", "password",hashed)
+    db.hset(f"user:{login}", "password", hashed)
     db.hset(f"user:{login}", "firstname", firstname)
     db.hset(f"user:{login}", "lastname", lastname)
     db.hset(f"user:{login}", "email", email)
@@ -86,13 +87,12 @@ def save_user(firstname, lastname, login, email, password, adress):
 
 
 def save_label(id, name, delivery_id, size):
-
     id = str(id)
     db.hset(f"label:{id}", "id", id)
     db.hset(f"label:{id}", "name", name)
     db.hset(f"label:{id}", "delivery_id", delivery_id)
     db.hset(f"label:{id}", "size", size)
-    db.hset(f"label:{id}", "sender", session.get('login'))
+    db.hset(f"label:{id}", "sender", g.authorization.get("usr"))
     return True
 
 
@@ -112,173 +112,210 @@ def verify_user(login, password):
 
 @app.route('/')
 def index():
-    if session.get('login') is None:
-        return render_template("index.html")
+    links = []
+    if g.authorization is None:
+        links.append(Link("login", "/sender/login"))
+        links.append(Link("register", "/sender/register"))
+        document = Document(data={}, links=links)
+        return document.to_json(), 200
 
-    return render_template('logged_index.html')
-
-
-@app.route('/sender/register', methods=['GET'])
-def registration_form():
-
-    if session.get('login') is None:
-        return render_template("registration.html")
-
-    return redirect(url_for('index'))
+    links.append(Link("dashboard", "/sender/dashboard"))
+    links.append(Link("add_label", "/label/add"))
+    document = Document(data={}, links=links)
+    return document.to_json(), 200
 
 
 @app.route('/sender/register', methods=['POST'])
 def registration():
-    firstname = request.form.get("firstname")
-    lastname = request.form.get("lastname")
-    adress = request.form.get("adress")
-    email = request.form.get("mail")
-    login = request.form.get("login")
-    password = request.form.get("password")
-    password2 = request.form.get("password2")
+    form_values = request.json
+    if form_values is None:
+        return {"error": "Brak JSON"}
+    firstname = form_values.get("firstname")
+    lastname = form_values.get("lastname")
+    adress = form_values.get("adress")
+    email = form_values.get("mail")
+    login = form_values.get("login")
+    password = form_values.get("password")
+    password2 = form_values.get("password2")
+
+    links = []
+    data = {}
+    links.append(Link("login", "/sender/login"))
+    links.append(Link("register", "/sender/register"))
 
     if not is_database_available():
-        flash("Błąd połączenia z bazą danych")
-        return redirect(url_for('registration_form'))
+        data["dbError"] = "Błąd połączenia z bazą danych"
+        document = Document(data=data, links=links)
+        return document.to_json(), 400
 
     if not firstname:
-        flash("Brak imienia")
+        data["firstnameError"] = "Brak imienia"
     if not lastname:
-        flash("Brak nazwiska")
+        data["lastnameError"] = "Brak nazwiska"
     if not adress:
-        flash("Brak adresu")
+        data["adressError"] = "Brak adresu"
     if not email:
-        flash("Brak adresu e-mail")
+        data["mailError"] = "Brak maila"
     if not login:
-        flash("Brak nazwy użytkownika")
+        data["loginError"] = "Brak loginu"
     if not password:
-        flash("Brak hasła")
+        data["passwordError"] = "Brak hasła"
     if password != password2:
-        flash(f"Hasła nie są takie same {password} _ {password2}")
-        return redirect(url_for('registration_form'))
+        data["passwordError"] = "Hasła nie są takie same"
+        document = Document(data=data, links=links)
+        return document.to_json(), 400
 
     if email and login and password and firstname and lastname and adress:
         if is_user(login):
-            flash(f"Użytkownik {login} istnieje")
-            return redirect(url_for('registration_form'))
+            data["loginError"] = "Taka nazwa użytkownika istnieje"
+            document = Document(data=data, links=links)
+            return document.to_json(), 400
     else:
-        return redirect(url_for('registration_form'))
+        document = Document(data=data, links=links)
+        return document.to_json(), 400
 
-    success = save_user(firstname,lastname,login,email,password,adress)
-
+    success = save_user(firstname, lastname, login, email, password, adress)
     if not success:
-        flash("Błąd rejestracji")
-        return redirect(url_for('registration_form'))
+        data["registrationError"] = "Wystąpił błąd podczas rejestracji. Spróbuj później"
 
-    return redirect(url_for('login_form'))
-
-
-@app.route('/sender/login', methods=["GET"])
-def login_form():
-
-    if session.get('login') is None:
-        return render_template("login.html")
-
-    return redirect(url_for('index'))
+    document = Document(data=data, links=links)
+    return document.to_json(), 200
 
 
 @app.route('/sender/login', methods=["POST"])
 def login():
-    login = request.form.get("login")
-    password = request.form.get("password")
+    form_values = request.json
+    if form_values is None:
+        return {"error": "Brak JSON"}
+
+    login = form_values.get("login")
+    password = form_values.get("password")
+
+    links = []
+    data = {}
+    links.append(Link("login", "/sender/login"))
+    links.append(Link("register", "/sender/register"))
 
     if not is_database_available():
-        flash("Błąd połączenia z bazą danych")
-        return redirect(url_for('login_form'))
+        data["dbError"] = "Błąd połączenia z bazą danych"
+        document = Document(data=data, links=links)
+        return document.to_json(), 400
 
     if not login or not password:
-        flash("Brak nazwy użytkownika lub hasła")
-        return redirect(url_for('login_form'))
-    if not verify_user(login,password):
-        flash("Błędna nazwa użytkownika i/lub hasła")
-        return redirect(url_for('login_form'))
+        data["formError"] = "Brak loginu lub hasła"
+        document = Document(data=data, links=links)
+        return document.to_json(), 400
 
-    session["login"] = login
-    session["logged-at"] = datetime.now()
+    if not verify_user(login, password):
+        data["loginError"] = "Błędny login lub hasło"
+        document = Document(data=data, links=links)
+        return document.to_json(), 400
 
-    return redirect(url_for('dashboard'))
+    links = []
+    links.append(Link("dashboard", "/sender/dashboard"))
+    links.append(Link("label:add", "/label/add"))
+    links.append(Link("logout", "/sender/logout"))
+    document = Document(data=data, links=links)
+    payload = {
+        "exp": datetime.utcnow() + timedelta(seconds=JWT_TIME),
+        "usr": login
+    }
+    token = encode(payload, JWT_SECRET, algorithm='HS256')
+    data["status"] = "logged"
+    data["token"] = token.decode()
+    document = Document(data=data, links=links)
+    return document.to_json(), 200
 
 
-@app.route('/sender/dashboard')
+@app.route('/sender/dashboard', methods=["GET"])
 def dashboard():
-
-    if session.get('login') is None:
-        flash("Najpierw musisz się zalogować")
-        return redirect(url_for('login_form'))
-
+    data = {}
+    links = []
     labels = {}
 
+    login = g.authorization.get("usr")
+
     for key in db.scan_iter("label:*"):
-        if(db.hget(key,"sender").decode()==session.get('login')):
-            labels[db.hget(key,"id").decode()]={
-                "id":db.hget(key,"id").decode(),
-                "name":db.hget(key,"name").decode(),
-                "delivery_id":db.hget(key,"delivery_id").decode(),
-                "size":db.hget(key,"size").decode()
+        if db.hget(key, "sender").decode() == login:
+            labels[db.hget(key, "id").decode()] = {
+                "id": db.hget(key, "id").decode(),
+                "name": db.hget(key, "name").decode(),
+                "delivery_id": db.hget(key, "delivery_id").decode(),
+                "size": db.hget(key, "size").decode()
             }
 
-    delete_tokens = {}
-
     for label in labels:
-        delete_tokens[label] = generate_delete_token(label, session.get('login')).decode()
+        links.append(Link(f"label:{label['id']}", f"/labels/{label['id']}"))
 
-    return render_template("dashboard.html", labels=labels.items(), haslabels=(len(labels)>0), delete_tokens=delete_tokens)
+    links.append(Link("label:add", "/label/add"))
+    links.append(Link("label:{la}", "/sender/logout"))
 
-
-@app.route('/label/add',methods=['GET'])
-def add_label_form():
-
-    if session.get('login') is None:
-        flash("Najpierw musisz się zalogować")
-        return redirect(url_for('login_form'))
-
-    return render_template("add_label.html")
+    data["labels"] = labels
+    data["login"] = login
+    document = Document(data=data, links=links)
+    return document.to_json(), 200
 
 
 @app.route('/label/add', methods=['POST'])
 def add_label():
-    name = request.form.get("name")
-    delivery_id = request.form.get("delivary_id")
-    size = request.form.get("size")
+    form_values = request.json
+    if form_values is None:
+        return {"error": "Brak JSON"}
+
+    data = {}
+    links = []
+    links.append(Link("dashboard", "/sender/dashboard"))
+    links.append(Link("label:add", "/label/add"))
+    links.append(Link("label:{la}", "/sender/logout"))
+
+    name = form_values.get("name")
+    delivery_id = form_values.get("delivery_id")
+    size = form_values.get("size")
     label_id = uuid4()
+    errors = []
 
     if not is_database_available():
-        flash("Błąd połączenia z bazą danych")
-        return redirect(url_for('add_label_form'))
+        data["dbError"] = "Błąd połączenia z bazą danych"
+        document = Document(data=data, links=links)
+        return document.to_json(), 400
 
     if not name:
-        flash("Brak danych odbiorcy")
-        return redirect(url_for('add_label_form'))
+        errors.append("Brak nazwy odbiorcy")
 
     if not delivery_id:
-        flash("Brak id punktu odbioru")
-        return redirect(url_for('add_label_form'))
+        errors.append("Brak id punktu odbioru")
 
     if not size:
-        flash("Brak wybranego rozmiaru")
-        return redirect(url_for('add_label_form'))
+        errors.append("Brak rozmiaru")
+
+    if len(errors) > 0:
+        document = Document(data=errors, links=links)
+        return document.to_json(), 400
 
     if name and delivery_id and size:
-        success = save_label(label_id,name,delivery_id,size)
+        success = save_label(label_id, name, delivery_id, size)
 
     if not success:
-        flash("Błąd tworzenia paczki")
-        return redirect(url_for('add_label_form'))
+        errors.append("Błąd tworzenia etykiety")
+        document = Document(data=errors, links=links)
+        return document.to_json(), 400
 
-    return redirect(url_for('dashboard'))
+    document = Document(links=links)
+    return document.to_json(), 200
 
 
 @app.route('/labels/<lid>', methods=["GET"])
 def show_label(lid):
+    errors=[]
+    links = []
+    links.append(Link("dashboard", "/sender/dashboard"))
+    links.append(Link("label:add", "/label/add"))
+    links.append(Link("label:{la}", "/sender/logout"))
 
-    if not db.hexists(f"label:{lid}","id"):
-        flash("Taka etykieta nie istnieje")
-        return redirect(url_for('index'))
+    if not db.hexists(f"label:{lid}", "id"):
+        errors.append("Taka etykieta nie istnieje")
+        document = Document(data=errors, links=links)
+        return document.to_json(), 400
 
     label = {
         "id": db.hget(f"label:{lid}", "id").decode(),
@@ -287,44 +324,34 @@ def show_label(lid):
         "size": db.hget(f"label:{lid}", "size").decode()
     }
 
-    return render_template("label.html", label_id=label['id'], name=label['name'], delivery=label['delivery_id'],size=label['size'])
+    document = Document(data=label, links=links)
+    return document.to_json(), 200
 
 
 @app.route('/label/delete/<lid>', methods=["GET"])
 def delete_label(lid):
-    token = request.args.get('token')
+    errors = []
+    links = []
+    links.append(Link("dashboard", "/sender/dashboard"))
+    links.append(Link("label:add", "/label/add"))
+    links.append(Link("label:{la}", "/sender/logout"))
 
-    if not db.hexists(f"label:{lid}","id"):
-        flash("Taka etykieta nie istnieje")
-        return redirect(url_for('index'))
+    if not db.hexists(f"label:{lid}", "id"):
+        errors.append("Taka etykieta nie istnieje")
+        document = Document(data=errors, links=links)
+        return document.to_json(), 400
 
-    if token is None:
-        flash("Nie masz dostępu do usunięcia etykiety")
-        return redirect(url_for('index'))
+    sender = db.hget(f"label:{lid}", "sender").decode()
 
-    try:
-        payload = decode(token, JWT_SECRET, algorithms=['HS256'], audience="label delete service")
-    except jwt.InvalidTokenError:
-        flash("Brak dostępu do usunięcia etykiety")
-        return redirect(url_for('index'))
-
-    if lid != payload.get('sub'):
-        flash("Błąd autoryzacji")
-        return redirect(url_for('index'))
+    if g.authorization.get("usr") != sender:
+        errors.append("Błąd autoryzacji")
+        document = Document(data=errors, links=links)
+        return document.to_json(), 400
 
     db.delete(f"label:{lid}")
 
-    return redirect(url_for('dashboard'))
-
-
-@app.route('/sender/logout')
-def sender_logout():
-
-    db.delete(f"session:{request.cookies.get('session')}")
-
-    session.clear()
-
-    return render_template("logout.html")
+    document = Document(links=links)
+    return document.to_json(), 200
 
 
 if __name__ == '__main__':
